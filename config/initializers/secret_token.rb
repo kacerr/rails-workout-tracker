b6b46d89ca19ebf369bbac6f8e6bdcd94485ac55
Honza Novak <kacerr.cz@gmail.com>
2014-02-04 07:52:05 -0500

    initial commit already containing user model, some bootstrap shit, but nothing big

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkoutTracker::Application.config.secret_key_base = '8e5fad50ad1aa84b666249cfb9c9b6bd9e58208e3f97698316c231e46d044a7b4a7969dcb17e5d6bc7e89dd2a0deaace342348d9cc7847d07032410dcebb8183'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorkoutTracker::Application.config.session_store :cookie_store, key: '_workoutTracker_session'
